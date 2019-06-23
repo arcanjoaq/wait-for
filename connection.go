c672cfccac0d2340656c6d52cf7cf8ac1b527b90
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2019-06-23 12:18:25 -0300

    Creating Generic Interface to the Database Connections and decoupling attempts from the test connection itself

---
+++ connection.go
@@ -0,0 +1,40 @@
+package main
+
+import ( 
+	"fmt" 
+	"time"
+	"log"
+)
+
+// DbConnection Interface for all Database Connections
+type DbConnection interface {
+	connect(host string, port int, user string, password string, database string) bool
+}
+
+func connectTo(dbtype string, host string, port int, user string, password string, database string, maxAttempts int, seconds int) (bool, error) {
+	t, err := getDbConnectionByType(dbtype)
+	if (err != nil) {
+		return false, err
+	}
+	connected := false
+	attempt := 1
+	for attempt <= maxAttempts && !connected {
+		connected = t.connect(host, port, user, password, database)
+		if (!connected) {
+			log.Printf("Attempt %d. Waiting for %d seconds...", attempt, seconds)
+			time.Sleep(time.Duration(seconds) * time.Second)
+		} else {
+			log.Println("Connected to the database")
+		}
+		attempt++
+	}
+	return connected, nil
+}
+
+func getDbConnectionByType(dbtype string) (DbConnection, error) {
+	if dbtype == "postgres" {
+		return PostgreSQLConnection{}, nil
+	}
+	return nil, fmt.Errorf("Invalid dbtype: %s", dbtype)
+}
+ 
--- wait-for-postgres.go
+++ wait-for-postgres.go
@@ -1,59 +1,29 @@
 package main
 
 import (
-	"flag"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 	"strings"
-	"time"
-
 	_ "github.com/lib/pq"
 )
 
-func connectToPostgreSQL(host string, port int, user string, password string, 
-	database string, maxAttempts int, seconds int) bool {
-	connected := false
-	attempt := 1
-	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, database)
+// PostgreSQLConnection Connector for PostgreSQL
+type PostgreSQLConnection struct {}
 
-	for attempt <= maxAttempts && !connected {
-		db, _ := sql.Open("postgres", dbinfo)
-		err := db.Ping()
-		if err != nil {
-			cause := "Unknown"
-			if strings.Contains(err.Error(), "connection refused") {
-				cause = "Connection Refused"
-			} else if strings.Contains(err.Error(), "connection reset by peer") {
-				cause = "Connection Reset"
-			}
-			log.Printf("Attempt %d. %s. Waiting for %d seconds...", attempt, cause, seconds)
-			time.Sleep(time.Duration(seconds) * time.Second)
-		} else {
-			log.Println("Connected to the database")
-			connected = true
+func (s PostgreSQLConnection) connect(host string, port int, user string, password string, database string) bool {
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+	db, _ := sql.Open("postgres", dbinfo)
+	err := db.Ping()
+	if err != nil {
+		cause := "Unknown"
+		if strings.Contains(err.Error(), "connection refused") {
+			cause = "Connection Refused"
+		} else if strings.Contains(err.Error(), "connection reset by peer") {
+			cause = "Connection Reset"
 		}
-		attempt++
-	}
-	return connected
-}
-
-func main() {
-	host := flag.String("host", "localhost", "Database host")
-	port := flag.Int("port", 5432, "Database port")
-	user := flag.String("user", "postgres", "Database user")
-	password := flag.String("password", "postgres", "Database password")
-	database := flag.String("database", "postgres", "Database name")
-
-	maxAttempts := flag.Int("maxAttempts", 3, "Maximum attempts")
-	seconds := flag.Int("seconds", 10, "Seconds to wait for Database")
-	
-	flag.Parse()
-
-	connected := connectToPostgreSQL(*host, *port, *user, *password, *database, *maxAttempts, *seconds)
-	if !connected {
-		os.Exit(1)
-	}
+		log.Printf("Error: %s", cause)
+		return false
+	} 
+	return true
 }
